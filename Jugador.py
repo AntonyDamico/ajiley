664a1364d05735b245fd1a0d6e3e74f1590d1eb2
Antony Damico <antonydamico29@gmail.com>
2018-05-01 19:30:49 -0400

    arreglando la falta del objeto mazo en la clase Jugador

--- Jugador.py
+++ Jugador.py
@@ -1,22 +1,19 @@
 from Mazo import Mazo
 from Carta import Carta
 
-mazo = Mazo()
-
-
 class Jugador:
     __mano = []
     __puntos = 0
 
-    def __init__(self):
+    def __init__(self, mazo):
         for _ in range(0, 3):
-            self.agarrarCarta()
+            self.agarrarCarta(mazo)
 
     def __calcularPuntos(self):
         for carta in self.__mano:
             self.__puntos += carta.getValor()
 
-    def agarrarCarta(self):
+    def agarrarCarta(self, mazo):
         nuevaCarta = mazo.agarrarCarta()
         self.__mano.append(nuevaCarta)
         self.__puntos += nuevaCarta.getValor()
@@ -29,16 +26,16 @@ class Jugador:
 
 # =========================================
 
-jugador1 = Jugador()
+# jugador1 = Jugador()
 
-for carta in jugador1.getMano():
-    print(carta.mostrarCarta())
+# for carta in jugador1.getMano():
+#     print(carta.mostrarCarta())
 
-print(jugador1.getPuntos())
+# print(jugador1.getPuntos())
 
-jugador1.agarrarCarta()
+# jugador1.agarrarCarta()
 
-for carta in jugador1.getMano():
-    print(carta.mostrarCarta())
+# for carta in jugador1.getMano():
+#     print(carta.mostrarCarta())
 
-print(jugador1.getPuntos())
+# print(jugador1.getPuntos())
