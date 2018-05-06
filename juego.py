d1f74d898ff8ad7412f5cbd04c9746fadf6c2e37
Antony Damico <antonydamico29@gmail.com>
2018-05-06 14:10:39 -0400

    agregando las cartas a una lista para cambiarlas

--- juego.py
+++ juego.py
@@ -4,6 +4,7 @@ from Jugador import Jugador
 
 mazo = Mazo()
 jugadores = []
+cartasSeleccionadas = []
 
 for i in range(0, 4):
     jugadores.append(Jugador(mazo))
@@ -31,9 +32,7 @@ for carta in jugadores[0].getMano():
     cartasImagenes.append(carta.getImg())
 
 
-# cardImg = pygame.image.load("imagenes/cartas/copa/1.jpg")
-
-def card(x, y, img):
+def imprimitCarta(x, y, img):
     img = pygame.image.load(img)
     gameDisplay.blit(img, (x, y))
 
@@ -44,6 +43,32 @@ def mostrarPuntos(puntos):
         texto = font.render(pinta + ": " + str(puntos[pinta]), True, blanco)
         gameDisplay.blit(texto, (anchoPantalla*0.65, (altoPantalla *0.75) + i))
         i += 20
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, negro)
+    return textSurface, textSurface.get_rect()
+
+
+def botonCarta(msg, x, y, w, h, carta):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    s = pygame.Surface((w,h))
+    s.fill((255,255,255))           
+
+    if x+w > mouse[0] > x and y + h > mouse[1] > y:
+        s.set_alpha(80)
+        gameDisplay.blit(s, (x,y))
+        if click[0] == 1:
+            if carta not in cartasSeleccionadas:
+                cartasSeleccionadas.append(carta)
+    else:
+        s.set_alpha(0)
+        gameDisplay.blit(s, (x,y))
+
+    smallText = pygame.font.Font('freesansbold.ttf', 20)
+    TextSurf, TextRect = text_objects(msg, smallText)
+    TextRect.center = ((x+(w/2)), (y+(h/2)))
+    gameDisplay.blit(TextSurf, TextRect)
 
 def gameLoop():
 
@@ -64,7 +89,15 @@ def gameLoop():
         gameDisplay.fill(verde)
 
         for i in range(0, len(cartasImagenes)):
-            card(x * (i + 1), y, cartasImagenes[i])
+            imprimitCarta(x * (i + 1), y, cartasImagenes[i])
+            if(i < len(cartasImagenes) - 1):
+                botonCarta("", x * (i + 1), y, 100 - (20), 139, jugadores[0].getMano()[i])
+            else:
+                botonCarta("", x * (i + 1), y, 100, 139, jugadores[0].getMano()[i])
+
+        for i in range(0, len(cartasSeleccionadas)):
+            print(cartasSeleccionadas[i].mostrarCarta())
+         
             
         mostrarPuntos(jugadores[0].getPuntos())
         
